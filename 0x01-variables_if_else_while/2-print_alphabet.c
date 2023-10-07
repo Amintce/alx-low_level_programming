91ddfd1aecf58186305d6b6943c6831fd43a8b41
amintce <akingbadeadeyemi5566@gmail.com>
2023-10-06 18:09:31 -0700

    task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - mian function
+ *
+ * Return: 0 when positive
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
